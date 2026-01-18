b85a30b36130af0f6fdd340ae5acb3a94c6b414d
RuchaKukreti <ruchakukreti@gmail.com>
2026-01-18 19:58:29 +0530

    Modularized code, used better naming and refactored ui messages

--- Function-New/guess_number.py
+++ Function-New/guess_number.py
@@ -2,26 +2,40 @@ import random
 
 LOWER_LIMIT = 1
 UPPER_LIMIT = 100
-
-def is_valid_guess(user_input):
-    return user_input.isdigit() and LOWER_LIMIT <= int(user_input) <= UPPER_LIMIT
-
-def compare_guess(guess, target):
-    is_correct_guess = False
-
-    if guess < target:
-        print("Too low. Guess again")
-    elif guess > target:
-        print("Too high. Guess again")
-    else:
-        is_correct_guess = True
-
-    return is_correct_guess
+LOW = "low"
+HIGH = "high"
+CORRECT = "correct"
 
 def get_user_guess():
     return input(f"Guess a number between {LOWER_LIMIT} and {UPPER_LIMIT}: ")
 
-def main():
+def is_valid_guess(user_input):
+    return user_input.isdigit() and LOWER_LIMIT <= int(user_input) <= UPPER_LIMIT
+
+def get_valid_guess():
+    while True:
+        guess=input(f"I won't count this one. Please enter a number between {LOWER_LIMIT} and {UPPER_LIMIT}: ")
+        if is_valid_guess(guess):
+            return guess
+
+def evaluate_guess(guess, target):
+    guess_result=""
+    if guess < target:
+        guess_result=LOW
+    elif guess > target:
+        guess_result= HIGH
+    else:
+        guess_result=CORRECT
+
+    return guess_result
+
+def display_guess_result(guess_result, number_of_guesses):
+    if guess_result == CORRECT:
+        print(f"You guessed it in {number_of_guesses} guesses!")
+    else:
+        print(f"Too {guess_result}. Guess again")
+
+def guess_the_number():
     target_number = random.randint(LOWER_LIMIT, UPPER_LIMIT)
     number_of_guesses = 0
 
@@ -29,18 +43,17 @@ def main():
 
     while True:
         if not is_valid_guess(guess):
-            guess = input(
-                "I won't count this one. Please enter a number between 1 and 100: "
-            )
-            continue
+            guess = get_valid_guess()
 
         number_of_guesses += 1
         guess = int(guess)
 
-        if compare_guess(guess, target_number):
-            print(f"You guessed it in {number_of_guesses} guesses!")
+        guess_result=evaluate_guess(guess, target_number)
+        display_guess_result(guess_result, number_of_guesses)
+        
+        if guess_result==CORRECT:
             break
-
+        
         guess = get_user_guess()
 
-main()
+guess_the_number()
